c6113604046cef94ecdd656d7ac7c1f00852a573
Shashank Srivastava <shankyonline02@gmail.com>
2021-07-20 15:37:09 +0530

    First commit Test

---
+++ TL4.py
@@ -0,0 +1,9 @@
+2 + 3
+
+print("shanky")
+
+print("shanky\'s laptop")
+
+# for line changing
+
+print('shanky \nlaptop')
---
+++ TL5.py
@@ -0,0 +1,20 @@
+# Variables in numbers
+x = 2
+y = 6
+z = x + y
+print(z)
+print(z+2)
+
+# Variables in string
+
+name = "Shanky"
+print(name)
+print(name + 'shashank')
+# Array in string
+print(name[1])
+print(name[-1])
+print(name[1:3])
+print(name[1:])
+print(name[:4])
+#len(name)
+print(len(name))
