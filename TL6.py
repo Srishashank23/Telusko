2a2310da796072b74538f2e03db11833748dbfaa
Shashank Srivastava <shankyonline02@gmail.com>
2021-08-01 17:03:47 +0530

    Tl6

--- TL6.py
+++ TL6.py
@@ -1,5 +1,48 @@
-nums = [2, 4, 6, 8]
+nums = [2, 4, 6, 8, 1, 5]
+# print(nums)
+# print(nums[0])
+# print(nums[2:3])
+# print(nums[-1])
+#
+# names = ['Shanky', 'Shivesh', 'Ankit', 'John']
+# print(names)
+#
+# mil = [nums, names]
+# print(mil)
+#
+# # Add the number in the end of the list
+# nums.append(12)
+# print(nums)
+#
+# #  Add the number in any place or in the middle of the list.
+# nums.insert(2, 14)
+# print(nums)
+#
+# # Remove the numkber from the list.
+# nums.remove(14)
+# print(nums)
+# #  To delete the value using index number
+# nums.pop(1)
+# print(nums)
+#
+# # To use pop without index number
+#
+# nums.pop()
+# print(nums)
+#
+# del nums[2:]
+# print(nums)
+#
+# #  To add multiple values
+# # nums.extend([22, 24, 26, 28])
+# # print(nums)
+#
+# # min (nums)
+# # print(nums)
+
+# sum(nums)
+sum(nums)
 print(nums)
-print(nums[0])
-print(nums[2:3])
-print(nums[-1])
+
+nums.sort()
+print(nums)
---
+++ TL7.py
@@ -0,0 +1,14 @@
+# # Tuple
+# tup = (2, 4, 6, 8)
+# print(tup)
+#
+# # tup[1]
+# # print(tup)
+#
+# tup.count(4)
+# print(tup)
+
+
+# Set
+S = {12, 14, 16,22,18,20,2}
+print(S)
